6c5f44f3a693618b57449865a1e769c747093c07
İsmail Çolak <ismail.colak@ogr.iuc.edu.tr>
2025-05-17 13:55:12 +0300

    episode-14

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -1,43 +1,62 @@
 using System;
 using UnityEngine;
 
-[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Knight : MonoBehaviour
 {
     public float walkSpeed = 3f;
+    public float walkStopRate = 0.2f;
     public DetectionZone attackZone;
     Rigidbody2D rb;
     TouchingDirections touchingDirections;
     Animator animator;
 
-    public enum WalkableDirection { right, left}
+    public enum WalkableDirection { right, left }
 
     private WalkableDirection _walkDirection;
     private Vector2 walkDirectionVector = Vector2.right;
+    private bool hasFlippedOnWall = false;
 
-    public WalkableDirection WalkDirection {  get { return _walkDirection; } 
-        set { 
-            if(_walkDirection != value)
+    public WalkableDirection WalkDirection
+    {
+        get { return _walkDirection; }
+        set
+        {
+            if (_walkDirection != value)
             {
                 gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
-            
-                if(value == WalkableDirection.right)
+
+                if (value == WalkableDirection.right)
                 {
                     walkDirectionVector = Vector2.right;
-                }else if ((value == WalkableDirection.left))
+                }
+                else if ((value == WalkableDirection.left))
                 {
                     walkDirectionVector = Vector2.left;
                 }
             }
-            
-            _walkDirection = value; }
+
+            _walkDirection = value;
+        }
     }
 
     public bool _hasTarget = false;
-    public bool HasTarget { get { return _hasTarget; } private set
+
+    public bool HasTarget
+    {
+        get { return _hasTarget; }
+        private set
         {
             _hasTarget = value;
             animator.SetBool(AnimationStrings.hasTarget, value);
+        }
+    }
+
+    public bool CanMove
+    {
+        get
+        {
+            return animator.GetBool(AnimationStrings.canMove);
         }
     }
 
@@ -54,10 +73,26 @@ public class Knight : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        rb.linearVelocity=new Vector2(walkSpeed* walkDirectionVector.x, rb.linearVelocity.y);
+        if (CanMove)
+        {
+            rb.linearVelocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.linearVelocity.y);
+
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(Mathf.Lerp(rb.linearVelocityX,0,walkStopRate),rb.linearVelocityY);
+        }
         if (touchingDirections.IsGrounded && touchingDirections.IsOnWall)
         {
-            FlipDirection();
+            if (!hasFlippedOnWall)
+            {
+                FlipDirection();
+                hasFlippedOnWall = true;
+            }
+        }
+        else
+        {
+            hasFlippedOnWall = false;
         }
     }
 
@@ -66,7 +101,8 @@ public class Knight : MonoBehaviour
         if (WalkDirection == WalkableDirection.right)
         {
             WalkDirection = WalkableDirection.left;
-        }else if(WalkDirection == WalkableDirection.left)
+        }
+        else if (WalkDirection == WalkableDirection.left)
         {
             WalkDirection = WalkableDirection.right;
         }
